eff17a6e8df1eee7c4b0e24c055ef4ec535a71eb
Aysegul Yonet <AysegulYonet@gmail.com>
2021-04-23 20:29:37 -0700

    chore(deploy):New Web App Config

--- dist/angular-app/src/app/build-specific/index.prod.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.externalModules = void 0;
-exports.externalModules = [];
-//# sourceMappingURL=index.prod.js.map
