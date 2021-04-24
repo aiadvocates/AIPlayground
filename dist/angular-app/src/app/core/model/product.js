2e9334d77ab8329546dcb0333aad3df0ec935c42
Aysegul Yonet <AysegulYonet@gmail.com>
2021-04-23 20:29:37 -0700

    chore(deploy):New Web App Config

--- dist/angular-app/src/app/core/model/product.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Product = void 0;
-class Product {
-}
-exports.Product = Product;
-//# sourceMappingURL=product.js.map
