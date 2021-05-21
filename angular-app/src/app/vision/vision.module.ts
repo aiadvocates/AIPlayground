dc5f3efe9a93fa00c10a6e57a39057937a4deb8c
Aysegul Yonet <ayyonet@microsoft.com>
2021-05-20 21:01:47 -0700

    Start before may 21

--- angular-app/src/app/vision/vision.module.ts
+++ angular-app/src/app/vision/vision.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VisionComponent } from './vision.component';
 
-
-
 @NgModule( {
   declarations: [ VisionComponent ],
   exports: [ VisionComponent ],
