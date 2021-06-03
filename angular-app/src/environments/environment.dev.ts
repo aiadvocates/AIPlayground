ed446de89badaaff345eec9395c3e97bd308da5c
Aysegul Yonet <AysegulYonet@gmail.com>
2021-06-03 16:48:50 -0700

    Merged May 21st

--- angular-app/src/environments/environment.dev.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  API: 'api',
-  cognitiveEndPoint: 'https://ai-blog-cognitive.cognitiveservices.azure.com/',
-  faceEndPoint: 'https://westus2.api.cognitive.microsoft.com/vision/v3.0/analyze',
-  faceKey: process.env.faceKey,
-  visionKey: process.env.visionKey,
-  visionEndPoint: 'https://westus2.api.cognitive.microsoft.com/vision/v3.0/analyze',
-};
