6d726289ce8696213d68a65c868b47838de30e84
Yönet <3523671+Yonet@users.noreply.github.com>
2021-06-17 23:48:15 -0700

    fix(env): add it back for actions

---
+++ angular-app/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  API: 'api',
+  cognitiveEndPoint: 'https://ai-blog-cognitive.cognitiveservices.azure.com/',
+  faceEndPoint: 'https://westus2.api.cognitive.microsoft.com/face/v1.0/detect',
+  faceKey: 'process.env.faceKey',
+  visionKey: 'process.env.visionKey',
+  visionEndPoint: 'https://westus2.api.cognitive.microsoft.com/vision/v3.2/analyze',
+};
